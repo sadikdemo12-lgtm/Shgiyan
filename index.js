9aec82e5ebab1ee70766b050ab659f4bf490417a
sadikdemo12-lgtm <sadikdemo12@gmail.com>
2026-06-05 09:32:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ reply সবসময় ছোট এবং natural হবে।
     
     const data = await response.json();
     const reply = data.choices[0].message.content.trim();
-    res.send(reply);
+    res.json({ reply: reply });
     
   } catch(err) {
-    res.send("ঠিক আছে");
+    res.json({ reply: "ঠিক আছে" });
   }
 });
 
